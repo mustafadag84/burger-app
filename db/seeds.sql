de34847089e0e041ec020b061c430ce44a72701d
Mustafa Dag <mustafaa_dag@hotmail.com>
2020-08-26 11:47:49 -0400

    functions added to config,controllers folders and datas added to db folder

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('cheese');
+INSERT INTO burgers (name) VALUES ('italian');
+INSERT INTO burgers (name, devoured) VALUES ('avocado', true);
+INSERT INTO burgers (name, devoured) VALUES ('sushi', true);
+INSERT INTO burgers (name, devoured) VALUES ('beacon', true);
+INSERT INTO burgers (name) VALUES ('tripple deck');
