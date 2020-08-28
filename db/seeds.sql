ec76411c8ae8677c0dcd1b0858431b7bda3c8cf6
Mustafa Dag <mustafaa_dag@hotmail.com>
2020-08-28 15:26:30 -0400

    seeds.sql updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (name) VALUES ('cheese');
-INSERT INTO burgers (name) VALUES ('italian');
+INSERT INTO burgers (name) VALUES ('salmon');
+INSERT INTO burgers (name) VALUES ('luther');
 INSERT INTO burgers (name, devoured) VALUES ('avocado', true);
-INSERT INTO burgers (name, devoured) VALUES ('sushi', true);
-INSERT INTO burgers (name, devoured) VALUES ('beacon', true);
-INSERT INTO burgers (name) VALUES ('tripple deck');
+INSERT INTO burgers (name, devoured) VALUES ('chili', true);
+INSERT INTO burgers (name, devoured) VALUES ('patty melt', true);
+INSERT INTO burgers (name) VALUES ('chicken burger');
